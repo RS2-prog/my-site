61b86feffdd844fcf6a1d7135e28704f5a853b7c
RS2 <sr71574m@gmail.com>
2025-02-06 20:20:13 +0900

    change: favicon

--- src/components/contents/PortfolioItem.tsx
+++ src/components/contents/PortfolioItem.tsx
@@ -26,30 +26,12 @@ const PortfolioItem: React.FC<PortfolioItemProps> = ({
       <div className="mt-4 flex space-x-4 h-6">
         {hasGithub && (
           <a href={githubLink} target="_blank" rel="noopener noreferrer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6 text-gray-700 hover:text-black"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 2C6.48 2 2 6.48 2 12c0 4.42 2.87 8.166 6.839 9.49.5.091.681-.217.681-.482 0-.237-.009-.866-.014-1.7-2.782.603-3.369-1.34-3.369-1.34-.455-1.158-1.11-1.466-1.11-1.466-.908-.62.068-.608.068-.608 1.004.071 1.532 1.031 1.532 1.031.892 1.529 2.341 1.088 2.91.833.091-.647.349-1.088.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.983 1.03-2.682-.103-.253-.447-1.268.098-2.64 0 0 .84-.269 2.75 1.025A9.52 9.52 0 0 1 12 6.837c.85.004 1.705.114 2.502.333 1.91-1.294 2.75-1.025 2.75-1.025.546 1.372.202 2.387.099 2.64.641.699 1.029 1.591 1.029 2.682 0 3.844-2.339 4.687-4.566 4.936.359.31.678.919.678 1.852 0 1.338-.012 2.417-.012 2.744 0 .268.18.577.687.481A10.003 10.003 0 0 0 22 12c0-5.52-4.48-10-10-10Z"
-                clipRule="evenodd"
-              />
-            </svg>
+            <img src="/static/icon/github-mark.png" alt="Github" className='w-5 h-5 mr-2 hover:scale-110 transition duration-500'/>
           </a>
         )}
         {hasQiita && (
           <a href={qiitaLink} target="_blank" rel="noopener noreferrer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6 text-green-500 hover:text-green-700"
-            >
-              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm.6-13H11v5.27l4.22 2.52.78-1.29-3.4-2.04V7z" />
-            </svg>
+            <img src="/static/icon/qiita-icon.png" alt="Github" className='w-5 h-5 mr-2 hover:scale-110 transition duration-500'/>
           </a>
         )}
       </div>
--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -54,14 +54,7 @@ const Contact = () => {
                 className="flex items-center text-black hover:text-gray-600 text-[16pt]"
                 rel="noopener noreferrer"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  viewBox="0 0 24 24"
-                  className="w-5 h-5 mr-2"
-                >
-                  <path d="M12 .5C5.73.5.5 5.74.5 12c0 5.1 3.29 9.41 7.86 10.95.58.1.79-.26.79-.58v-2.17c-3.22.7-3.9-1.57-3.9-1.57-.53-1.34-1.3-1.7-1.3-1.7-1.07-.73.08-.72.08-.72 1.18.08 1.8 1.2 1.8 1.2 1.05 1.8 2.75 1.28 3.43.98.1-.76.41-1.28.74-1.57-2.57-.3-5.28-1.29-5.28-5.73 0-1.27.46-2.31 1.2-3.13-.12-.3-.52-1.52.11-3.17 0 0 .97-.31 3.18 1.2a10.86 10.86 0 0 1 5.8 0c2.2-1.51 3.17-1.2 3.17-1.2.63 1.65.23 2.87.12 3.17.74.82 1.2 1.86 1.2 3.13 0 4.44-2.72 5.43-5.3 5.72.42.36.8 1.08.8 2.18v3.23c0 .32.21.69.8.58A10.98 10.98 0 0 0 23.5 12c0-6.26-5.23-11.5-11.5-11.5z"/>
-                </svg>
+                <img src="/static/icon/github-mark.png" alt="Github" className='w-5 h-5 mr-2'/>
                 GitHub
               </a>
               
@@ -69,17 +62,10 @@ const Contact = () => {
               <a
                 href="https://qiita.com/Ryu_S_2025"
                 target="_blank"
-                className="flex items-center text-black hover:text-green-600 text-[16pt]"
+                className="flex items-center text-black  hover:text-gray-600 text-[16pt]"
                 rel="noopener noreferrer"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-5 h-5 mr-2"
-                >
-                  <path d="M12.003 0c-6.624 0-12 5.373-12 12 0 6.625 5.373 12 12 12 6.625 0 12-5.372 12-12 0-6.625-5.373-12-12-12zM12 20c-4.416 0-8-3.584-8-8 0-4.417 3.584-8 8-8s8 3.583 8 8c0 4.416-3.584 8-8 8z"/>
-                </svg>
+                <img src="/static/icon/qiita-icon.png" alt="Github" className='w-5 h-5 mr-2'/>
                 Qiita
               </a>
             </div>
